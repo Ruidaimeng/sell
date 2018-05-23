9e893b4306246ca47c209fad466a7f5ef283817f
Ruimeng <13816570705@163.com>
2018-05-23 13:25:58 +0800

    增加属性值

--- src/test/java/com/imooc/sell/repository/Demo1.java
+++ src/test/java/com/imooc/sell/repository/Demo1.java
@@ -1,5 +1,8 @@
 package com.imooc.sell.repository;
 
+import java.math.BigDecimal;
+
+import lombok.Data;
 
 /**
 * @ClassName: Demo1
@@ -8,6 +11,13 @@ package com.imooc.sell.repository;
 * @date 2018年5月23日 上午11:11:51
 *
 */ 
+@Data
 public class Demo1 {
+	
+	private String name ;
+	 
+	private Integer age ;
+	
+	private BigDecimal price ;
 
 }
