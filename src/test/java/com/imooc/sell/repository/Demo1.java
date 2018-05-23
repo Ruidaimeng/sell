eb1f13727e546c4b4ef7df466123c0d3cb63650c
Ruimeng <13816570705@163.com>
2018-05-23 11:13:42 +0800

    demo测试提交到GitHub上

---
+++ src/test/java/com/imooc/sell/repository/Demo1.java
@@ -0,0 +1,13 @@
+package com.imooc.sell.repository;
+
+
+/**
+* @ClassName: Demo1
+* @Description: 测试提交新增类到GitHub上
+* @author ruimeng
+* @date 2018年5月23日 上午11:11:51
+*
+*/ 
+public class Demo1 {
+
+}
